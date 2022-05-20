8a369888b214abf355747263f5e6401c9bc3a55a
SilvioMVieira <silvio.vieira@ba.estudante.senai.br>
2022-05-20 17:02:27 -0300

    section meio trabalhos

--- resources/views/PetHappy/abrigo.blade.php
+++ resources/views/PetHappy/abrigo.blade.php
@@ -20,70 +20,41 @@
                 </div>
                 <div class="card equipe">
                     <h4 class="p-2 text-dark mt-4">Nossa Equipe</h4>
-                    <!-- <div id="carouselExampleControl" class="carousel slide carousel-fade" data-bs-ride="carousel">
+                    <div class="slide">
+                        <div id="carouselExampleSlidesOnly" class="fotoEquipe carousel slide carousel-fade" data-bs-ride="carousel" style="width: 300px; height: 200px">
                             <div class="carousel-inner">
                                 <div class="carousel-item active" data-bs-interval="2000">
-                                    <img src="../img/abrigo/atuacao/atuacao1.jpeg" id="imgBanner" class="d-block w-75 me-0" alt="Quem Somos">
+                                    <img src="img/abrigo/elon-musk.jpg" class="d-block w-100" alt="...">
+                                    <div class="carousel-caption d-flex d-md-block">
+                                        <p class="text-light bg-primary badge bg-primary text-wrap">Guigas Musk</p>
+                                    </div>
                                 </div>
                                 <div class="carousel-item" data-bs-interval="2000">
-                                    <img src="../img/abrigo/atuacao/atuacao2.jpg" id="imgBanner" class="d-block w-75 me-0" alt="Agência">
-                                </div>
-                                <div class="carousel-item" data-bs-interval="2000">
-                                    <img src="../img/abrigo/atuacao/atuacao3.jpg" id="imgBanner" class="d-block w-75 me-0" alt="Marketing">
-                                </div>
-                                <div class="carousel-item" data-bs-interval="2000">
-                                    <img src="../img/abrigo/atuacao/atuacao4.jpg" id="imgBanner" class="d-block w-75 me-0" alt="Contato">
-                                </div>
-                            </div>
-                        </div> -->
-                    <div id="carouselExampleControls" class="carousel slide carousel-fade" data-bs-ride="carousel">
-                        <div class="carousel-inner">
-                            <div class="carousel-item active">
-                                <img class="imgpessoa rounded" src="img/abrigo/rosto1.png" class="d-block" alt="...">
-                                <div class="carousel-caption d-flex d-md-block">
-                                    <p class="text-light bg-primary badge bg-primary text-wrap">Maria Silva</p>
-                                </div>
-                            </div>
-                            <div class="carousel-item">
-                                <img class="imgpessoa rounded" src="img/abrigo/rosto2.png" class="d-block" alt="...">
-                                <div class="carousel-caption d-flex d-md-block">
-                                    <p class="text-light bg-primary badge bg-primary text-wrap">Tereza Batista</p>
-                                </div>
-                            </div>
-                            <div class="carousel-item">
-                                <img class="imgpessoa rounded" src="img/abrigo/rosto3.jpg" class="d-block" alt="...">
-                                <div class="carousel-caption d-flex d-md-block">
-                                    <p class=" caption text-light bg-primary badge bg-primary text-wrap">João Santos</p>
+                                    <img src="img/abrigo/Bill_Gates_.jpg" class="d-block w-100" alt="...">
+                                    <div class="carousel-caption d-flex d-md-block">
+                                        <p class="text-light bg-primary badge bg-primary text-wrap">Daniel Gates</p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-
-                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span class="visually-hidden">Previous</span>
-                        </button>
-                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span class="visually-hidden">Next</span>
-                        </button>
-                    </div><br>
+                    </div><br><br>
                     <div>
                         <h6 class="text-center text-dark">Localização</h6>
-                        <iframe class="mapa border shadow-sm p-1 bg-body rounded rounded-3" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3899.4316346625205!2d-38.95097168561534!3d-12.219026748454738!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7143903dbbf1785%3A0x7843ea34be97c354!2sPatinhas%20Pet%20Hostel!5e0!3m2!1spt-BR!2sbr!4v1652742476067!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        <div class="local">
+                            <iframe class="mapa border shadow-sm p-1 bg-body rounded rounded-3 w-100" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3899.4316346625205!2d-38.95097168561534!3d-12.219026748454738!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7143903dbbf1785%3A0x7843ea34be97c354!2sPatinhas%20Pet%20Hostel!5e0!3m2!1spt-BR!2sbr!4v1652742476067!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        </div>
                     </div>
                     <div class="card-body">
                         <h6 class="card-title text-center text-dark">Horário de Funcionamento</h6>
-                        <div class="horario bg-light border border-0 w-75 mx-auto">
+                        <div class="horario bg-light mx-auto">
                             <div class="slide">
-                                <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel" data-interval="500">
+                                <div id="carouselExampleSlidesOnly" class="horaFunc carousel slide w-100 mx-auto" data-bs-ride="carousel" data-interval="500">
                                     <div class="carousel-inner">
                                         <div class="carousel-item active" data-bs-interval="2000">
                                             <p class="card-text text-center">Segunda a Sexta-Feira: de 8 às 17h</p>
-                                            <!-- <img src="..." class="d-block w-100" alt="..."> -->
                                         </div>
                                         <div class="carousel-item" data-bs-interval="2000">
                                             <p class="card-text text-center">Sábado: de 8 às 12h</p>
-                                            <!-- <img src="..." class="d-block w-100" alt="..."> -->
                                         </div>
                                     </div>
                                 </div>
@@ -104,16 +75,16 @@
                         <div id="carouselExampleControl" class="carousel slide carousel-fade" data-bs-ride="carousel">
                             <div class="carousel-inner">
                                 <div class="carousel-item active" data-bs-interval="2000">
-                                    <img src="../img/abrigo/atuacao/atuacao1.jpeg" id="imgBanner" class="d-block w-75 me-0" alt="Quem Somos">
+                                    <img src="../img/abrigo/atuacao/atuacao1.jpeg" id="imgBanner" class="d-block w-100 me-0" alt="Quem Somos">
                                 </div>
                                 <div class="carousel-item" data-bs-interval="2000">
-                                    <img src="../img/abrigo/atuacao/atuacao2.jpg" id="imgBanner" class="d-block w-75 me-0" alt="Agência">
+                                    <img src="../img/abrigo/atuacao/atuacao2.jpg" id="imgBanner" class="d-block w-100 me-0" alt="Agência">
                                 </div>
                                 <div class="carousel-item" data-bs-interval="2000">
-                                    <img src="../img/abrigo/atuacao/atuacao3.jpg" id="imgBanner" class="d-block w-75 me-0" alt="Marketing">
+                                    <img src="../img/abrigo/atuacao/atuacao3.jpg" id="imgBanner" class="d-block w-100 me-0" alt="Marketing">
                                 </div>
                                 <div class="carousel-item" data-bs-interval="2000">
-                                    <img src="../img/abrigo/atuacao/atuacao4.jpg" id="imgBanner" class="d-block w-75 me-0" alt="Contato">
+                                    <img src="../img/abrigo/atuacao/atuacao4.jpg" id="imgBanner" class="d-block w-100 me-0" alt="Contato">
                                 </div>
                             </div>
                         </div>
@@ -140,16 +111,16 @@
                         <div id="carouselExampleControl" class="carousel slide carousel-fade" data-bs-ride="carousel">
                             <div class="carousel-inner">
                                 <div class="carousel-item active" data-bs-interval="2000">
-                                    <img src="../img/abrigo/atuacao/atuacao1.jpeg" id="imgBanner" class="d-block w-75 ms-0" alt="Quem Somos">
+                                    <img src="../img/abrigo/atuacao/atuacao1.jpeg" id="imgBanner" class="d-block w-100 ms-0" alt="Quem Somos">
                                 </div>
                                 <div class="carousel-item" data-bs-interval="2000">
-                                    <img src="../img/abrigo/atuacao/atuacao2.jpg" id="imgBanner" class="d-block w-75 ms-0" alt="Agência">
+                                    <img src="../img/abrigo/atuacao/atuacao2.jpg" id="imgBanner" class="d-block w-100 ms-0" alt="Agência">
                                 </div>
                                 <div class="carousel-item" data-bs-interval="2000">
-                                    <img src="../img/abrigo/atuacao/atuacao3.jpg" id="imgBanner" class="d-block w-75 ms-0" alt="Marketing">
+                                    <img src="../img/abrigo/atuacao/atuacao3.jpg" id="imgBanner" class="d-block w-100 ms-0" alt="Marketing">
                                 </div>
                                 <div class="carousel-item" data-bs-interval="2000">
-                                    <img src="../img/abrigo/atuacao/atuacao4.jpg" id="imgBanner" class="d-block w-75 ms-0" alt="Contato">
+                                    <img src="../img/abrigo/atuacao/atuacao4.jpg" id="imgBanner" class="d-block w-100 ms-0" alt="Contato">
                                 </div>
                             </div>
                         </div>
@@ -164,16 +135,16 @@
                         <div id="carouselExampleControl" class="carousel slide carousel-fade" data-bs-ride="carousel">
                             <div class="carousel-inner">
                                 <div class="carousel-item active" data-bs-interval="2000">
-                                    <img src="../img/abrigo/atuacao/atuacao1.jpeg" id="imgBanner" class="d-block w-75 me-0" alt="Quem Somos">
+                                    <img src="../img/abrigo/atuacao/atuacao1.jpeg" id="imgBanner" class="d-block w-100 me-0" alt="Quem Somos">
                                 </div>
                                 <div class="carousel-item" data-bs-interval="2000">
-                                    <img src="../img/abrigo/atuacao/atuacao2.jpg" id="imgBanner" class="d-block w-75 me-0" alt="Agência">
+                                    <img src="../img/abrigo/atuacao/atuacao2.jpg" id="imgBanner" class="d-block w-100 me-0" alt="Agência">
                                 </div>
                                 <div class="carousel-item" data-bs-interval="2000">
-                                    <img src="../img/abrigo/atuacao/atuacao3.jpg" id="imgBanner" class="d-block w-75 me-0" alt="Marketing">
+                                    <img src="../img/abrigo/atuacao/atuacao3.jpg" id="imgBanner" class="d-block w-100 me-0" alt="Marketing">
                                 </div>
                                 <div class="carousel-item" data-bs-interval="2000">
-                                    <img src="../img/abrigo/atuacao/atuacao4.jpg" id="imgBanner" class="d-block w-75 me-0" alt="Contato">
+                                    <img src="../img/abrigo/atuacao/atuacao4.jpg" id="imgBanner" class="d-block w-100 me-0" alt="Contato">
                                 </div>
                             </div>
                         </div>
@@ -200,16 +171,16 @@
                         <div id="carouselExampleControl" class="carousel slide carousel-fade" data-bs-ride="carousel">
                             <div class="carousel-inner">
                                 <div class="carousel-item active" data-bs-interval="2000">
-                                    <img src="../img/abrigo/atuacao/atuacao1.jpeg" id="imgBanner" class="d-block w-75 ms-0" alt="Quem Somos">
+                                    <img src="../img/abrigo/atuacao/atuacao1.jpeg" id="imgBanner" class="d-block w-100 ms-0" alt="Quem Somos">
                                 </div>
                                 <div class="carousel-item" data-bs-interval="2000">
-                                    <img src="../img/abrigo/atuacao/atuacao2.jpg" id="imgBanner" class="d-block w-75 ms-0" alt="Agência">
+                                    <img src="../img/abrigo/atuacao/atuacao2.jpg" id="imgBanner" class="d-block w-100 ms-0" alt="Agência">
                                 </div>
                                 <div class="carousel-item" data-bs-interval="2000">
-                                    <img src="../img/abrigo/atuacao/atuacao3.jpg" id="imgBanner" class="d-block w-75 ms-0" alt="Marketing">
+                                    <img src="../img/abrigo/atuacao/atuacao3.jpg" id="imgBanner" class="d-block w-100 ms-0" alt="Marketing">
                                 </div>
                                 <div class="carousel-item" data-bs-interval="2000">
-                                    <img src="../img/abrigo/atuacao/atuacao4.jpg" id="imgBanner" class="d-block w-75 ms-0" alt="Contato">
+                                    <img src="../img/abrigo/atuacao/atuacao4.jpg" id="imgBanner" class="d-block w-100 ms-0" alt="Contato">
                                 </div>
                             </div>
                         </div>
